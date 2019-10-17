6c2041940b0ec56fe24a29bb04b4c153cc477bf8
geethatulasi-nyros <geethatulasi.nyros@gmail.com>
2019-10-17 09:07:08 +0530

    local repo

---
+++ data.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+class Data extends Component {
+	render(){
+		return(
+			<View>
+				<Text>react native</Text>
+			</View>
+		);
+	}
+}
+export default Data;
